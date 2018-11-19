8d27e8ca83c215c65ff2eafbd2f024ea425037ab
Timothée Oliger <oliger.timothee@gmail.com>
2018-11-19 10:09:30 +0100

    init

---
+++ app/src/components/Home.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+  <section class="home-page"><h1>home-page Component</h1></section>
+</template>
+
+<script lang="js">
+  export default  {
+    name: 'home-page',
+    props: [],
+    mounted() {
+
+    },
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+</script>
+
+<style scoped lang="less">
+.home-page {
+}
+</style>
