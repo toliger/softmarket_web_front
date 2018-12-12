7812c07b63553d435a93acb413032960301f9faf
Timothée Oliger <oliger.timothee@gmail.com>
2018-12-12 12:54:03 +0100

    init

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Vue from "vue";
+import "./plugins/vuetify";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import { createProvider } from "./vue-apollo";
+import BootstrapVue from "bootstrap-vue";
+
+Vue.use(BootstrapVue);
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  apolloProvider: createProvider(),
+  render: h => h(App)
+}).$mount("#app");
---
+++ src/views/Home.vue
@@ -0,0 +1,44 @@
+<template lang="html">
+  <section id="home-page">
+    <v-container> <h1>Catégories</h1>
+      <ul>
+        <li v-for="c in categories" v-bind:key="c.name">
+          <p><span class="cname">{{c.name}}</span>  {{c.description}}</p>
+        </li>
+      </ul>
+    </v-container>
+  </section>
+</template>
+
+<script lang="js">
+import GETCATEGORIES from "@/graphql/GetCategories.gql";
+  export default  {
+    name: 'home-page',
+    props: [],
+    mounted() {
+
+    },
+    data() {
+      return {
+        categories: []
+      }
+    },
+    apollo: {
+      categories: GETCATEGORIES
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+</script>
+
+<style scoped lang="less">
+#home-page {
+  .cname {
+    font-size: 2em;
+  }
+}
+</style>
